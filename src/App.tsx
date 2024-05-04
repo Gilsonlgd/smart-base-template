bd6be8545390034717f64e0f04f04aea1d89ee9a
Gilsonlgd <garciadasilvagilson@gmail.com>
2024-05-04 17:33:28 -0300

    📦 feat: Add  bootstrap and react-bootstrap packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { RouterProvider } from 'react-router-dom';
 import router from './router';
 
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
+
 function App(): JSX.Element {
   return <RouterProvider router={router} />;
 }
