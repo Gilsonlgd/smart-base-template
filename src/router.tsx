19dbee7b06224f1304ff0bc7458113ef4937f890
Gilsonlgd <garciadasilvagilson@gmail.com>
2024-05-23 11:44:37 -0300

    🏗️ feat: Update router structure

--- src/router.tsx
+++ src/router.tsx
@@ -1,4 +1,6 @@
 import { createBrowserRouter } from 'react-router-dom';
+
+import PublicLayout from '@layout/public/PublicLayout';
 import publicRoutes from './routes/publicRoutes';
 
 import FallbackView from './views/Fallback';
@@ -6,7 +8,13 @@ import FallbackView from './views/Fallback';
 const router = createBrowserRouter([
   {
     errorElement: <FallbackView />,
-    children: [...publicRoutes],
+    children: [
+      {
+        path: '/',
+        element: <PublicLayout />,
+        children: publicRoutes,
+      },
+    ],
   },
 ]);
 
--- src/routes/publicRoutes.tsx
+++ src/routes/publicRoutes.tsx
@@ -1,13 +1,5 @@
-import PublicLayout from '@layout/public/PublicLayout';
-
 import Home from '@views/public/Home/Home';
 
-const publicRoutes = [
-  {
-    path: '/',
-    element: <PublicLayout />,
-    children: [{ path: '/', element: <Home /> }],
-  },
-];
+const publicRoutes = [{ path: '/', element: <Home /> }];
 
 export default publicRoutes;
