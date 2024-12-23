61f7ef86d2719862c1a736e1833eb5b120cdd837
Gilsonlgd <garciadasilvagilson@gmail.com>
2024-12-23 14:57:18 -0300

    🔧 feat: Add vite-tsconfig-paths plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,9 @@
 import { defineConfig, PluginOption } from 'vite';
-import * as path from 'path';
 import react from '@vitejs/plugin-react-swc';
 import { reactScopedCssPlugin } from 'rollup-plugin-react-scoped-css';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), reactScopedCssPlugin() as PluginOption],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-      '@assets': path.resolve(__dirname, 'src/assets'),
-      '@components': path.resolve(__dirname, 'src/components'),
-      '@views': path.resolve(__dirname, 'src/views'),
-      '@layout': path.resolve(__dirname, 'src/layout'),
-      '@services': path.resolve(__dirname, 'src/services'),
-    },
-  },
+  plugins: [react(), tsconfigPaths(), reactScopedCssPlugin() as PluginOption],
 });
